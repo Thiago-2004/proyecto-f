e653f9dba149532db1180b25addba4d61eba0358
thiago-ricon <thiagoricon987@gmail.com>
2024-08-13 21:34:37 -0300

    narbar

--- proyecto-f/src/App.jsx
+++ proyecto-f/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+import NavbarCustom from './componentes/narbar/narbar';  // Importa el componente de la barra de navegación
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className='contenedor'>
+        <NavbarCustom /> 
+        <main>
+          <Routes>
+            <Route path="/" element={<Navigate to="/Fumitecno" />} />
+            {/* <Route path='/argentum' element={<Seccion />} />
+            <Route path='/productos' element={<Productos />} />
+            <Route path='/contacto' element={<ContactForm />} />
+            <Route path='/carrito' element={<Carrito />} /> */}
+           
+          </Routes>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more 
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
